30052a088ea43e700549d8ebecb66ea75bc2957f
Ivan Liutko <ivanliutko@gmail.com>
2021-01-06 15:25:53 +0200

    Initial Commit

---
+++ MostWantedVerbs/MostWantedVerbsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MostWantedVerbsApp.swift
+//  MostWantedVerbs
+//
+//  Created by Ivan Liutko on 06.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct MostWantedVerbsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
